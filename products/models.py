9b8c2faea2278e10f71573944fbacdbecfb56527
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-02-13 14:53:30 +0000

    bug-fix: price in Product model was incorrectly named float

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=20)
-    float = models.FloatField()
+    price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
 
