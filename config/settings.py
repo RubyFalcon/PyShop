cb48acabd87bd2c0f5a4c10440975fbdcf0e1b29
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-03-13 16:40:29 +0000

    refactor: rename Django project package to config
    Renamed the main Django project module from 'pyshop' to 'config'
    to better separate project configuration from application code.
    Updated settings, WSGI/ASGI configuration, and manage.py imports.

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pyshop.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pyshop.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
